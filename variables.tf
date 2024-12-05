b579c3c4575759163d4aabe38bbc64f19d3da01c
harishkrishna5 <harishkrish09@gmail.com>
2024-12-05 11:36:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Name of the existing EC2 key pair"
-  default = "new-key"
+  default = "keypair"
 }
 
 variable "s3_bucket_name" {
